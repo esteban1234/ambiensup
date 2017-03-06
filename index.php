a2ecf2b6e14949143f36cd6f7dd9b5f5e7193700
esteban1234 <tva_libra@hotmail.com>
2017-03-06 14:27:15 -0600

    index listo

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<?php include 'vistas/encabezado.php' ?>
+<body>
+	<article class="contenedorpag">
+		<header>
+			<img src="img/ambiensup.png" alt="AMBIENSUP ANALISTAS PROFESIONALES" class="img-responsive pull-left">
+
+			<div class="col-md-8 pull-right menu">
+				<ul class="nav nav-tabs">
+					<li role="presentation" class="active"><a href="index.php">INICIO</a></li>
+					<li role="presentation"><a href="vistas/nosotros.php">CONOCENOS</a></li>
+					<li role="presentation"><a href="vistas/servicios.php">SERVICIOS</a></li>
+					<li role="presentation"><a href="vistas/contacto.php">CONTACTO</a></li>
+				</ul>
+			</div>
+		</header>
+
+      		<div class="jumbotron jum">
+			  <article class="contenjum">
+			  	<h2>AMBIENSUP ANALISTAS PROFESIONALES</h2>
+			  	<p>Somos una empresa con años de experiencia dedicada a resolver problemas Geofísicos y Ambientales.</p>
+			  	<p><a class="btn btn-danger btn-lg" href="#" role="button"><i class="fa fa-volume-control-phone" aria-hidden="true"></i>&nbsp;Contáctanos</a></p>
+			  </article>
+			</div>
+
+			<div class="hola">
+				<h1 class="colorh2 text-center">Soluciones Geofísicas</h1> <br>
+		        <!-- <div class="row"> -->
+		          <div class="col-md-4">
+		            <img id="imggeo" src="img/geotecnia.jpg"
+		            class="img-responsive">
+		            <h2 class="colorh2">Geotécnia</h2>
+		            <p class="colorp">
+		            	<b><i class="fa fa-check-square-o" aria-hidden="true"></i>&nbsp;Down Hole</b> <br>
+		            	<b><i class="fa fa-check-square-o" aria-hidden="true"></i>&nbsp;Cross Hole</b> <br>
+		            	<b><i class="fa fa-check-square-o" aria-hidden="true"></i>&nbsp;Pruebas de continuidad</b> <br>
+		            	<b><i class="fa fa-check-square-o" aria-hidden="true"></i>&nbsp;Tomografia Sismica</b>
+		            </p>
+		          </div>
+		          <div class="col-md-4">
+		            <img id="imggeo" src="img/mag.jpg"
+		            class="img-responsive">
+		            <h2 class="colorh2">Métodos electromagnéticos</h2>
+		            <p class="colorp">
+		            	<b><i class="fa fa-check-square-o" aria-hidden="true"></i>&nbsp;Georradar GPR</b> <br>
+		            	<b><i class="fa fa-check-square-o" aria-hidden="true"></i>&nbsp;Transitorio Electromagnético TEM</b>
+		            </p>
+		          </div>
+		          <div class="col-md-4">
+		            <img id="imggeo" src="img/elec.jpg"
+		            class="img-responsive">
+		            <h2 class="colorh2">Métodos eléctricos</h2>
+		            <p class="colorp">
+		            	<b><i class="fa fa-check-square-o" aria-hidden="true"></i>&nbsp;Tomografía Eléctrica</b> <br>
+		            	<b><i class="fa fa-check-square-o" aria-hidden="true"></i>&nbsp;Sondeo Eléctrico Vertical</b> <br>
+		            	<b><i class="fa fa-check-square-o" aria-hidden="true"></i>&nbsp;Sondeo Eléctrico Vertical Wenner</b>
+		            </p>
+		          </div>
+		        <!-- </div> -->
+
+            </div>
+			
+			<br>
+            <article class="boton">
+            	<a class="btn btn-danger btn-lg" href="#" role="button"><i class="fa fa-volume-control-phone" aria-hidden="true"></i>&nbsp;Servicios</a>
+            	   <!-- <span class="glyphicon glyphicon-star" aria-hidden="true"></span> Star -->
+            </article>
+
+		<?php include 'vistas/pie.php' ?>
+
+	</article>
+
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.js"></script>
+</body>
+</html>
---
+++ vistas/pie.php
@@ -0,0 +1,39 @@
+<footer>
+      <!-- <div class="container"> -->
+        <!-- <div class="row"> -->
+          <div class="col-sm-8">
+            <h3>AMBIENSUP ANALISTAS PROFESIONALES S.A DE C.V</h3>
+            <address>
+  <strong>Dirección.</strong><br>
+  C. 11-A #327 por 60 y 38-A<br>
+  Fracc. del Norte, Mérida, Yucatán<br>
+  <strong>Teléfono:</strong> PENDIENTE <br>
+  <strong>Correo:</strong><a href="mailto:info@pendiente.com"> info@pendiente.com</a> <br>
+  <strong>Horario:</strong> Lunes a Viernes de 9am a 6pm
+</address>
+          </div>
+          <div class="col-sm-4">
+            <p class="text-info text-right">
+              <br>
+              <br>
+            </p>
+            <div class="row">
+              <div class="col-md-12 hidden-lg hidden-md hidden-sm text-left">
+                <a href="#"><i class="fa fa-3x fa-fw fa-instagram text-inverse"></i></a>
+                <a href="#"><i class="fa fa-3x fa-fw fa-twitter text-inverse"></i></a>
+                <a href="#"><i class="fa fa-3x fa-fw fa-facebook text-inverse"></i></a>
+                <a href="mailto:info@pendiente.com"><i class="fa fa-3x fa-fw fa-envelope-open-o text-inverse"></i></a>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-12 hidden-xs text-right">
+                <a href="#"><i class="fa fa-3x fa-fw fa-instagram text-inverse"></i></a>
+                <a href="#"><i class="fa fa-3x fa-fw fa-twitter text-inverse"></i></a>
+                <a href="#"><i class="fa fa-3x fa-fw fa-facebook text-inverse"></i></a>
+                <a href="mailto:info@pendiente.com"><i class="fa fa-3x fa-fw fa-envelope-open-o text-inverse"></i></a>
+              </div>
+            </div>
+          </div>
+        <!-- </div> -->
+      <!-- </div> -->
+    </footer>
