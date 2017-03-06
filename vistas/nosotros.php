3365f114c0140a31a2215d1fd2d9398ca1daaf90
esteban1234 <tva_libra@hotmail.com>
2017-03-06 17:32:38 -0600

    mitad de quienes somos

--- vistas/nosotros.php
+++ vistas/nosotros.php
@@ -17,9 +17,61 @@
 
 		<ul class="breadcrumb text-right submenu">
 		  <li><a href="../index.php">Inicio</a> <span class="divider"></span></li>
-		  <!-- <li><a href="#">Library</a> <span class="divider"></span></li> -->
 		  <li class="active">Quienes Somos</li>
 		</ul>
+
+		<article class="contennosotros">
+			<h1 class="colorh2">Conocenos</h1>
+	        <div class="row">
+	          <div class="col-md-6">
+	            <img class="imgmedio" src="../img/medioambiente.jpg"
+	            class="img-responsive">
+	          </div>
+	          <div class="col-md-6">
+	            <p class=" text-nosotros colorp">
+	              Somos una empresa dedicada a la producción de análisis científicos del subsuelo, otorgando servicios de geofísica, medio ambiente y arquitectura, brindando soluciones efectivas al mejor costo para nuestros clientes.
+	              <br><br>
+
+	              Promoveemos e impulsamos iniciativas en desarrollo sustentable a través de nuestros servicios, promoviendo el cuidado y conservación del medio ambiente. Somos una empresa conformada por profesionales y expertos en múltiples disciplinas entre las que se destacan Especialistas en Sistemas de Información, Geólogos, Ingenieros Geólogos, Geofísicos, Ingenieros de Petróleos, Ingenieros de Sistemas, Ingenieros Electrónicos, Físicos,  entre otros;
+	            </p>
+	          </div>
+	        </div>
+		</article>
+
+        <div class="row">
+          <div class="col-md-3">
+            <img src="http://pingendo.github.io/pingendo-bootstrap/assets/placeholder.png"
+            class="img-responsive">
+            <h2 class="colorh2">Misión</h2>
+            <p class="colorp">
+            	Ofrecer servicios de consultoría al sector ambiental, servicos de levantamiento geofísico y servicos de arquitectura con la mas alta calidad y apegados a estandares internacionales atendiendo las necesidades de nuestros clientes de forma rápida, profesional y con enfoque humanista.
+            </p>
+          </div>
+          <div class="col-md-3">
+            <img src="http://pingendo.github.io/pingendo-bootstrap/assets/placeholder.png"
+            class="img-responsive">
+            <h2 class="colorh2">Visión</h2>
+            <p class="colorp">Lorem ipsum dolor sit amet, consectetur adipisici elit,
+              <br>sed eiusmod tempor incidunt ut labore et dolore magna aliqua.
+              <br>Ut enim ad minim veniam, quis nostrud</p>
+          </div>
+          <div class="col-md-3">
+            <img src="http://pingendo.github.io/pingendo-bootstrap/assets/placeholder.png"
+            class="img-responsive img-rounded">
+            <h2 class="colorh2">Objetivo</h2>
+            <p class="colorp">Lorem ipsum dolor sit amet, consectetur adipisici elit,
+              <br>sed eiusmod tempor incidunt ut labore et dolore magna aliqua.
+              <br>Ut enim ad minim veniam, quis nostrud</p>
+          </div>
+          <div class="col-md-3">
+            <img src="http://pingendo.github.io/pingendo-bootstrap/assets/placeholder.png"
+            class="img-responsive">
+            <h2 class="colorh2">Valores</h2>
+            <p class="colorp">Lorem ipsum dolor sit amet, consectetur adipisici elit,
+              <br>sed eiusmod tempor incidunt ut labore et dolore magna aliqua.
+              <br>Ut enim ad minim veniam, quis nostrud</p>
+          </div>
+        </div>
 
 <?php include 'pie.php' ?>
 
